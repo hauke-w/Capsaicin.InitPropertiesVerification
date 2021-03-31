89260614b161f8f732bba3c5d030fbf098ef2b8f
Hauke Wittern <hauke.wittern@minebea-intec.com>
2021-03-31 14:06:34 +0200

    fixed generated verfification code

--- Capsaicin.InitPropertiesVerification.Generator/PartialClassWithInitPropertiesGenerator.ExecuteContext.cs
+++ Capsaicin.InitPropertiesVerification.Generator/PartialClassWithInitPropertiesGenerator.ExecuteContext.cs
@@ -108,7 +108,7 @@ namespace {namespaceName}
                 foreach (var propertyName in localPropertyNames)
                 {
                     source.Append($@"
-            if (nameof({propertyName}) is null)
+            if ({propertyName} is null)
             {{
                 return nameof({propertyName});
             }}
